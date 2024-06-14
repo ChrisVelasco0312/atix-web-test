2f5e54b9a88dac7ed07d506666b453a6670f1801
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2024-06-13 22:20:20 -0500

    [FEATURE] Main theme config

---
+++ src/components/test-comp.tsx
@@ -0,0 +1,11 @@
+const TestComp = () => {
+  return (
+    <div>
+      <p className="bg-pureBlue font-integralCF text-[32px]">Test</p>
+      <p className="bg-pureBlue font-jakartaSansBold text-[32px]">Test</p>
+      <p className="bg-pureBlue font-jakartaSansRegular text-[32px]">Test</p>
+    </div>
+  );
+};
+
+export default TestComp;
