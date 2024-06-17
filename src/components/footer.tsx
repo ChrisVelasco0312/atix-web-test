bb9855d510b1bebb7f2e6c34c9dec92846833230
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2024-06-17 09:44:21 -0500

    [FIX] Delete non important files

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,50 @@
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
-      <div>
-        <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+    <footer className="flex justify-between h-[480px] px-[60px] pt-[80px] bg-petroleo500 text-white p-4 bg-base-300 text-base-content">
+      <div className="flex flex-col gap-10">
+        <img
+          className="w-[103px] h-[42px]"
+          src="logos/atix-logo-white.svg"
+          alt="atix-logo"
+        />
+        <p className="w-[292px] leading-[150%]">
+          Our mission is to to help you enhance the effectiveness of your
+          organization.
+        </p>
+        <div className="flex">
+          <img
+            className="h-[63px] w-[63px]"
+            src="icons/Facebook.svg"
+            alt="facebook"
+          />
+          <img
+            className="h-[63px] w-[63px]"
+            src="icons/Twitter.svg"
+            alt="twitter"
+          />
+          <img
+            className="h-[63px] w-[63px]"
+            src="icons/Instagram.svg"
+            alt="instagram"
+          />
+        </div>
+      </div>
+      <div className="flex gap-[90px]">
+        <div className="flex flex-col gap-4">
+          <h1>Products</h1>
+          <ul>
+            <li>SEIZ</li>
+            <li>Notery</li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1>About</h1>
+          <ul>
+            <li>Quienes somos</li>
+            <li>Por qué lo hacemos</li>
+            <li>Meet our team</li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
--- src/components/publications-content.tsx
+++ src/components/publications-content.tsx
@@ -1,6 +1,38 @@
+import SectionTitle from "./section-title";
+
 const PublicationsContent = () => (
-  <div>
-    <h1>Publications</h1>
+  <div className="relative h-[481px] grid justify-center">
+    <SectionTitle title="PUBLICATIONS" iconColor="#1C7FC1" topDistance="54px" />
+    <div className="grid grid-flow-col gap-x-10 place-items-center">
+      <div className="grid justify-center w-[351px] gap-4">
+        <img
+          className="w-[351px] h-[126px]"
+          src="logos/cience-direct-logo.png"
+          alt="cience-direct-logo"
+        />
+        <a
+          href="https://www.sciencedirect.com/science/article/abs/pii/S095070512030589X?dgcid=author"
+          className="text-center text-[#545A7C] underline"
+        >
+          “Incremental learning model inspired in Rehearsal for deep
+          convolutional networks”
+        </a>
+      </div>
+      <div className="grid justify-center w-[351px] gap-4">
+        <img
+          className="w-[344px] h-[99px]"
+          src="logos/springer-link-logo.png"
+          alt="springer-link-logo"
+        />
+        <a
+          href="https://link.springer.com/chapter/10.1007/978-3-319-92898-2_10"
+          className="text-center text-[#545A7C] underline"
+        >
+          “Document Management System Based on a Private Blockchain for the
+          Support of the Judicial Embargoes Process in Colombia”
+        </a>
+      </div>
+    </div>
   </div>
 );
 
