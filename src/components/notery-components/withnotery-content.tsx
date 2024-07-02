58ec6b7eec67330568b81d8912c5392ada29d4c7
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2024-07-02 11:51:45 -0500

    [FEATURE] add withnotery content

--- src/components/notery-components/withnotery-content.tsx
+++ src/components/notery-components/withnotery-content.tsx
@@ -1,7 +1,9 @@
 const WithNoteryContent = () => {
   return (
-    <article className="h-[316px]">
-
+    <article className="h-[316px] grid content-center justify-items-center">
+      <h1 className="text-white text-[24px] text-center w-[900px]">Simply tell <span className="ml-4">
+        <img className="inline relative bottom-[10px]" src="logos/logo-notery-white.svg" alt="Notery Logo" />
+      </span> <strong>, your document management assistant,</strong> which data it needs to collect  from your documents</h1>
     </article>
   );
 };
