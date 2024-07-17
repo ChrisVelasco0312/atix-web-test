fae6d0c99da78508d3ea2dcc1af04bb331bb877a
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2024-07-16 21:18:11 -0500

    [FEATURE] Initial mobila adaptation

--- src/components/index-components/about-content.tsx
+++ src/components/index-components/about-content.tsx
@@ -3,16 +3,16 @@ import SectionTitle from "../shared/section-title";
 
 const AboutContent = () => {
   return (
-    <article className="relative grid grid-cols-2 w-full">
+    <article className="relative grid lg:grid-cols-2 content-center w-full lg:h-[420px] h-[636px]">
       <SectionTitle
         title="ABOUT ATIX DIGITAL"
         iconColor="#1C7FC1"
         topDistance="54px"
       />
       <div />
-      <div className="py-[119px]">
-        <h1 className="font-integralCF text-purple500 text-[40px] leading-[98%]">
-          We are proud to be solution oriented not problem oriented
+      <div className="w-[341px] lg:w-[606px] lg:h-[147px]">
+        <h1 className="font-integralCF text-purple500 lg:text-[40px] text-[28px] lg:leading-[98%] leading-[115%]">
+          We are proud to be solution-oriented, problem-oriented
         </h1>
         <Button style={{ marginTop: "40px" }} text="Get to know us" />
       </div>
--- src/components/shared/section.tsx
+++ src/components/shared/section.tsx
@@ -1,9 +1,12 @@
 import type React from "react";
+// import { useEffect } from "react";
+// import type { ViewportInfo } from "../../utils/viewportInfo";
 
 interface SectionProps {
   background: string;
   additionalBg?: string;
   additionalStyles?: React.CSSProperties;
+  additionalBgClassName?: string;
   backgroundColor?: string;
   heightMode: "h-auto" | "h-screen" | "h-fit";
   contentHeight: string;
@@ -14,15 +17,32 @@ const Section = ({
   background,
   additionalBg = "",
   additionalStyles = {},
+  additionalBgClassName = "",
   heightMode = "h-auto",
   contentHeight = "100%",
   children,
-  backgroundColor = "none",
+  backgroundColor = "transparent",
 }: SectionProps) => {
 
+  // useEffect(() => {
+  //   const wrapper = document.querySelector("#viewport-wrapper") as HTMLElement;
+  //   if (wrapper) {
+  //     const handleViewportChange = (event: Event) => {
+  //       const customEvent = event as CustomEvent<ViewportInfo>;
+  //       console.log("customEvent", customEvent.detail);
+  //     };
+  //
+  //     wrapper.addEventListener("viewport-change", handleViewportChange);
+  //
+  //     return () => {
+  //       wrapper.removeEventListener("viewport-change", handleViewportChange);
+  //     }
+  //   }
+  // }, []);
+  //
   const container = (content: React.ReactNode) => (
     <section
-      className={`w-full ${heightMode} min-w-[1440px] flex flex-col items-center`}
+      className={`w-full ${heightMode} lg:min-w-[1440px] flex flex-col items-center`}
       style={{
         backgroundImage: background && `url(${background})`,
         backgroundSize: "cover",
@@ -34,18 +54,17 @@ const Section = ({
   );
 
   const content = (
-    <div className={`w-[1440px] h-[${contentHeight}]`}>{children}</div>
+    <div className={`lg:w-[1440px] h-[${contentHeight}]`}>{children}</div>
   );
 
   if (additionalBg.length > 0) {
     return container(
       <div
-        className={`w-full ${heightMode} min-w-[1440px] flex flex-col items-center`}
+        className={`${additionalBgClassName} w-full ${heightMode}  flex flex-col items-center`}
         style={{
           backgroundImage: `url(${additionalBg})`,
           backgroundSize: "contain",
           backgroundColor,
-          ...additionalStyles
         }}>
         {content}
       </div>
