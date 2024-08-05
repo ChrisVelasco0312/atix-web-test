aabe5aaf3de95855460d127c3c4256a749d1a770
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2024-08-05 01:18:01 -0500

    [FEATURE] Add welcome page translation

--- src/components/index-components/welcome-content.tsx
+++ src/components/index-components/welcome-content.tsx
@@ -1,4 +1,5 @@
 import Button from "../shared/button";
+import { t } from "astro-i18n";
 
 const WelcomeContent = () => {
   return (
@@ -6,25 +7,23 @@ const WelcomeContent = () => {
       <div />
       <div className="relative lg:p-0">
         <h6 className="text-white mb-[10px] tracking-[2.8px]">
-          WELCOME TO ATIX
+          {t("index.welcome.title")}
         </h6>
         {/*ADD OUTLINE*/}
         <h1
           className="absolute text-stroke-white opacity-20 lg:left-[10px] left-[5px] lg:top-[38px] font-integralCF text-white lg:text-[76px] text-[52px] leading-[98%]"
         >
-          SMART <br /> BUSINESS <br /> SOLUTIONS
+          {t("index.welcome.slogan.line1")} <br /> {t("index.welcome.slogan.line2")} <br /> {t("index.welcome.slogan.line3")}
         </h1>
         <h1
           className="relative z-10 font-integralCF text-white lg:text-[76px] text-[52px] leading-[98%]"
         >
-          SMART <br /> BUSINESS <br /> SOLUTIONS
+          {t("index.welcome.slogan.line1")} <br /> {t("index.welcome.slogan.line2")} <br /> {t("index.welcome.slogan.line3")}
         </h1>
         <p className="text-white text-[18px] mt-[26px] leading-[150%] lg:w-[449px]">
-          We digitize & automate administrative processes with modern
-          technologies, to help you enhance the effectiveness of your
-          organization{" "}
+          {t('index.welcome.description')}{" "}
         </p>
-        <Button style={{ marginTop: "40px" }} text="Learn more" />
+        <Button style={{ marginTop: "40px" }} text={t('index.welcome.learnMore')} />
       </div>
     </article>
   );
