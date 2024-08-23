e7578e2724d47636b88b5af1ee524358fbb9929f
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2024-08-23 12:53:01 -0500

    [FIX] notery welcome page spaces and size

--- src/components/notery-components/intro-content.tsx
+++ src/components/notery-components/intro-content.tsx
@@ -18,19 +18,23 @@ const IntroContent = () => {
       <article
         className="relative w-full lg:h-screen h-[960px] grid lg:grid-cols-2 content-center items-center justify-center pl-[20px] lg:p-0"
       >
-        <div className="grid gap-4">
+        <div className="grid gap-4 lg:relative lg:bottom-[100px] 2xl:right-[30px]">
           <img className="w-[170px] lg:w-auto" src="../../logos/logo-notery-white.svg" alt="Notery Logo" />
           <div className="grid gap-4 text-white">
-            <h1 className="font-integralCF text-[40px] lg:w-auto w-[90%]">
+            <h1 className="font-integralCF text-[40px] lg:w-auto w-[90%] lg:leading-normal leading-[140%]">
               <span className="text-purple600">{t("notery.intro.title1")}</span> {t("notery.intro.title2")} <br className="hidden lg:block" />
               {t("notery.intro.title3")} <span className="text-purple600">{t("notery.intro.title4")}</span> <br className="hidden lg:block" />
               <span className="text-purple600">{t("notery.intro.title5")}</span> {t("notery.intro.title6")}
             </h1>
-            <p className="text-[18px] lg:w-[465px] w-[98%]">
+            <p className="lg:text-[18px] lg:w-[465px] w-[98%]">
               {t("notery.intro.description")}
             </p>
           </div>
-          <img className="lg:hidden block" src="../../images/notery-intro.png" alt="Notery Intro" />
+          <img
+            className="lg:hidden block relative top-[11%] scale-[1.08]"
+            src="../../images/notery-intro.png"
+            alt="Notery Intro"
+          />
         </div>
       </article>
     </>
